770548c0387f7fb45b29a9d0004b4b48da7d675f
ljgarcia <leylajael@gmail.com>
2015-03-20 14:34:36 +0000

    Add and update json schema files.

--- snippets/proteinCategoryFTViewer_test.js
+++ snippets/proteinCategoryFTViewer_test.js
@@ -6,85 +6,84 @@ var sequence = "abcdefghikklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijk"
 sequence = var_j3kp33.sequence;
 
 var domainsAndSites = {
-    category: "Domains and sites",
-    types: [
+    "category": "Domains and sites",
+    "types": [
         {
-            type: "ACT_SITE", label: "Active site", cvid: "SO:0000417",
-            locations: [
+            "type": "ACT_SITE", "label": "Active site", "cvId": "SO:0000417",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        { ftid: "act-site-1", description: "act-site-1-desc", begin: 5, end:6}
-                        ,{ ftid: "act-site-2", description: "act-site-2-desc", begin: 10, end: 10}
-                        ,{ ftid: "act-site-3", description: "act-site-3-desc", begin: sequence.length-10, end: sequence.length-10}
-                        //,{ ftid: "act-site-4", description: "act-site-4-desc", begin: 116, end: 116}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "act-site-1", "description": "act-site-1-desc", "begin": 5, "end":6}
+                        ,{ "ftId": "act-site-2", "description": "act-site-2-desc", "begin": 10, "end": 10}
+                        ,{ "ftId": "act-site-3", "description": "act-site-3-desc", "begin": sequence.length-10, "end": sequence.length-10}
                     ]
                 }
             ]
         },
         {
-            type: "SITE", label: "Site", cvid: "SO:0000SSS",
-            locations: [
+            "type": "SITE", "label": "Site", "cvId": "SO:0000SSS",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        { ftid: "site-1", description: "site-1-desc", begin: 20, end:20}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "site-1", "description": "site-1-desc", "begin": 20, "end":20}
                     ]
                 }
             ]
         },
         {
-            type: "METAL", label: "Metal", cvid: "SO:0000MMM",
-            locations: [
+            "type": "METAL", "label": "Metal", "cvId": "SO:0000MMM",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        { ftid: "metal-1", description: "metal-1-desc", begin: 30, end:30}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "metal-1", "description": "metal-1-desc", "begin": 30, "end":30}
                     ]
                 }
             ]
         },
         {
-            type: "BINDING", label: "Binding", cvid: "SO:0000BBB",
-            locations: [
+            "type": "BINDING", "label": "Binding", "cvId": "SO:0000BBB",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        { ftid: "binding-1", description: "binding-1-desc", begin: 40, end:41}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "binding-1", "description": "binding-1-desc", "begin": 40, "end":41}
                     ]
                 }
             ]
         },
         {
-            type: "LIPID", label: "Lipid", cvid: "SO:0000LLL",
-            locations: [
+            "type": "LIPID", "label": "Lipid", "cvId": "SO:0000LLL",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        { ftid: "lipid-1", description: "lipid-1-desc", begin: 50, end:50}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "lipid-1", "description": "lipid-1-desc", "begin": 50, "end":50}
                     ]
                 }
             ]
         },
         {
-            type: "NON_STD", label: "Non standard", cvid: "SO:0000NNN",
-            locations: [
+            "type": "NON_STD", "label": "Non standard", "cvId": "SO:0000NNN",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        { ftid: "non-std-1", description: "non-std-1-desc", begin: 60, end:60}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "non-std-1", "description": "non-std-1-desc", "begin": 60, "end":60}
                     ]
                 }
             ]
         },
         {
-            type: "CA_BIND", label: "Calcium binding", cvid: "SO:0001080",
-            locations: [
+            "type": "CA_BIND", "label": "Calcium binding", "cvId": "SO:0001080",
+            "locations": [
                 {
-                    locationType: "CONTINUOUS",
-                    features: [
-                        { ftid: "ca_bind-1", description: "ca_bind-1-desc", begin: 7, end: 14},
-                        { ftid: "ca_bind-2", description: "ca_bind-2-desc", begin: 38, end: 45}
+                    "locationType": "CONTINUOUS",
+                    "features": [
+                        { "ftId": "ca_bind-1", "description": "ca_bind-1-desc", "begin": 7, "end": 14},
+                        { "ftId": "ca_bind-2", "description": "ca_bind-2-desc", "begin": 38, "end": 45}
                     ]
                 }
             ]
@@ -92,73 +91,72 @@ var domainsAndSites = {
     ]
 };
 var structural = {
-    category: "Structural features - Seq info",
-    types: [
+    "category": "Structural features - Seq info",
+    "types": [
         {
-            type: "DISULFID", label: "Disulfide bond", cvid: "MOD:00689",
-            locations: [
+            "type": "DISULFID", "label": "Disulfide bond", "cvId": "MOD:00689",
+            "locations": [
                 {
-                    locationType: "BRIDGE",
-                    features: [
-                        { ftid: "bond-1", description: "bond-1-desc", begin: 15, end: 25}
+                    "locationType": "BRIDGE",
+                    "features": [
+                        { "ftId": "bond-1", "description": "bond-1-desc", "begin": 15, "end": 25}
                     ]
                 }
             ]
         },
         {
-            type: "TURN", label: "turn", cvid: "SO:0001128",
-            locations: [
+            "type": "TURN", "label": "turn", "cvId": "SO:0001128",
+            "locations": [
                 {
-                    locationType: "CONTINUOUS",
-                    features: [
+                    "locationType": "CONTINUOUS",
+                    "features": [
                         {
-                            ftid: "turn-1", description: "turn-1-desc", begin: 8, end: 59,
-                            evidence: {type: "ECO:0000218", source: {ref: "23"}}
+                            "ftId": "turn-1", "description": "turn-1-desc", "begin": 8, "end": 59,
+                            "evidence": [{"type": "ECO:0000218", "source": {"ref": "23"}}]
                         },
                         {
-                            ftid: "turn-2", description: "turn-2-desc", begin: 18, end: 69,
-                            evidence: {type: "ECO:0000203", source: {dbReferenceType: "MIM", dbReferenceId: "123"}}
+                            "ftId": "turn-2", "description": "turn-2-desc", "begin": 18, "end": 69,
+                            "evidence": [{"type": "ECO:0000203", "source": {"dbReferenceType": "MIM", "dbReferenceId": "123"}}]
                         },
                         {
-                            ftid: "turn-3", description: "turn-3-desc", begin: 28, end: 79,
-                            evidence: {type: "ECO:0000203", source: {dbReferenceType: "MIM", dbReferenceId: "123"}}
+                            "ftId": "turn-3", "description": "turn-3-desc", "begin": 28, "end": 79,
+                            "evidence": [{"type": "ECO:0000203", "source": {"dbReferenceType": "MIM", "dbReferenceId": "123"}}]
                         }
                     ]
                 }
             ]
         },
         {
-            type: "NON_TER", label: "Non terminal residue", cvid: "NA",
-            locations: [
+            "type": "NON_TER", "label": "Non terminal residue", "cvId": "NA",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        {ftid: "non-ter-0", description: "non-ter-0-desc", id: "internal ID", begin: sequence.length, end: sequence.length}
+                    "locationType": "POSITION",
+                    "features": [
+                        {"ftId": "non-ter-0", "description": "non-ter-0-desc", "internalId": "internal ID", "begin": sequence.length, "end": sequence.length}
                     ]
                 }
             ]
 
         },
         {
-            type: "NON_CONS", label: "Non consecutive residue", cvid: "NA",
-            locations: [
+            "type": "NON_CONS", "label": "Non consecutive residue", "cvId": "NA",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        { ftid: "non-cons-0", description: "non-cons-0-desc", begin: 21, end: 21}
-                        //,{ ftid: "non-cons-1", description: "non-cons-1-desc", begin: 113, end: 113}
-                        ,{ ftid: "non-cons-2", description: "non-cons-1-desc", begin: sequence.length-10, end: sequence.length-10}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "non-cons-0", "description": "non-cons-0-desc", "begin": 21, "end": 21}
+                        ,{ "ftId": "non-cons-2", "description": "non-cons-1-desc", "begin": sequence.length-10, "end": sequence.length-10}
                     ]
                 }
             ]
         },
         {
-            type: "INIT_MET", label: "Removed initiator methionine", cvid: "NA",
-            locations: [
+            "type": "INIT_MET", "label": "Removed initiator methionine", "cvId": "NA",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        { ftid: "init-met-0", description: "init-met-0-desc", begin: 1, end: 1}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "init-met-0", "description": "init-met-0-desc", "begin": 1, "end": 1}
                     ]
                 }
             ]
@@ -166,29 +164,27 @@ var structural = {
     ]
 };
 var ptms = {
-    category: "Post translational modification",
-    types: [
+    "category": "Post translational modification",
+    "types": [
         {
-            type: "CARBOHYD", label: "Glycosylation", cvid: "MOD:00693",
-            locations: [
+            "type": "CARBOHYD", "label": "Glycosylation", "cvId": "MOD:00693",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        { ftid: "carbohyd-0", description: "carbohyd-0-desc", begin: 7, end: 7},
-                        { ftid: "carbohyd-1", description: "carbohyd-1-desc", begin: 52, end: 53}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "carbohyd-0", "description": "carbohyd-0-desc", "begin": 7, "end": 7},
+                        { "ftId": "carbohyd-1", "description": "carbohyd-1-desc", "begin": 52, "end": 53}
                     ]
                 }
             ]
         },
         {
-            type: "MOD_RES", label: "Modified residue", cvid: "SO:0001089",
-            locations: [
+            "type": "MOD_RES", "label": "Modified residue", "cvId": "SO:0001089",
+            "locations": [
                 {
-                    locationType: "POSITION",
-                    features: [
-                        //{ ftid: "mod_res-1", description: "mod_res-1-desc", begin: 103, end: 103}
-                        //,
-                        { ftid: "mod_res-2", description: "mod_res-2-desc", begin: 40, end: 40}
+                    "locationType": "POSITION",
+                    "features": [
+                        { "ftId": "mod_res-2", "description": "mod_res-2-desc", "begin": 40, "end": 40}
                     ]
                 }
             ]
@@ -238,5 +234,5 @@ var instance = new pftv_aux_proteinCategoryFTViewer({
     useTooltips: true
     ,collapsible: true
     ,sequence: model.sequence
-    ,category: model.categories[1]//.types[0]
+    ,"category": model.categories[1]//.types[0]
 });
