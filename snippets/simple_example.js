2001349e71ab4ffd55a1d56872d530f5ffab21f2
ljgarcia <ljgarcia@ebi.ac.uk>
2015-07-03 16:47:02 +0100

    Add event support for feature selection

--- snippets/simple_example.js
+++ snippets/simple_example.js
@@ -1,3 +1,7 @@
 // if you don't specify a html file, the sniper will generate a div
 var app = require("biojs-vis-proteinFeaturesViewer");
 var instance = new app({el: yourDiv, text: 'biojs', uniprotacc : 'P05067'});
+instance.getDispatcher().on("featureSelected", function(obj) {
+ console.log('catching');
+ console.log(obj);
+ });
