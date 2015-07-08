2265e7fb27cfeec0ee045043cd4fe9fcb8ee8751
ljgarcia <ljgarcia@ebi.ac.uk>
2015-07-08 17:06:38 +0100

    Add "ready" event to FeaturesViewer.

--- snippets/simple_example.js
+++ snippets/simple_example.js
@@ -1,6 +1,9 @@
 // if you don't specify a html file, the sniper will generate a div
 var app = require("biojs-vis-proteinFeaturesViewer");
 var instance = new app({el: yourDiv, text: 'biojs', uniprotacc : 'P05067'});
+instance.getDispatcher().on("ready", function() {
+ console.log('ready');
+});
 instance.getDispatcher().on("featureSelected", function(obj) {
  console.log('catching');
  console.log(obj);
