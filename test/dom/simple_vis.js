1bf53860548337b7f3c4d034130bf296dd8afd6e
ljgarcia <ljgarcia@ebi.ac.uk>
2015-07-08 17:08:55 +0100

    Test initial div structure.

--- test/dom/simple_vis.js
+++ test/dom/simple_vis.js
@@ -11,22 +11,31 @@ var chai = require('chai');
 
 // @see http://chaijs.com/api/assert/
 var assert = chai.assert;
+// register alternative styles
+// @see http://chaijs.com/api/bdd/
+chai.expect();
+chai.should();
 
 // this is your global div instance (see index.html)
 var yourDiv = document.getElementById('mocha');
 
 // requires your main app (specified in index.js)
-var biojs_vis_proteinFeaturesViewer = require('../..');
+var FeaturesViewer = require('../..');
 
-describe('biojs_vis_proteinFeaturesViewer module', function() {
-	var opts = {
-		target: "mocha",
-		uniprotacc: 'P05067'
-	};
-	var instance = new biojs_vis_proteinFeaturesViewer(opts);
-	it('should fill the textBox', function() {
-		//instance.greetings("biojs");
-		// biojs_vis_proteinFeaturesViewer.greetings('mocha', 'biojs');
-		// assert.equal(yourDiv.innerHTML,"hello biojs");
+describe('FeaturesViewer module', function() {
+    var opts = {
+        el: yourDiv,
+        uniprotacc: 'P05067'
+    };
+    var instance = new FeaturesViewer(opts);
+	it('should create general structure', function() {
+        instance.getDispatcher().on("ready", function() {
+            var mochaDiv = document.getElementById('mocha');
+            assert(mochaDiv.firstElementChild.getAttribute('class'), 'fv-container');
+            assert(mochaDiv.childElementCount, 1);
+            var mainContainer = document.getElementsByClassName('fv-container');
+            assert(mainContainer.length, 1);
+            assert(mainContainer[0].childElementCount, 4);
+        });
 	});
 });
