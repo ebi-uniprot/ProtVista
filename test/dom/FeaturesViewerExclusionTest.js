90724d445550994ef526c1ffc4d2fd50e3a48a84
ljgarcia <leylajael@gmail.com>
2015-07-23 11:43:51 +0100

    Update tests to category exclusion.

--- test/dom/FeaturesViewerExclusionTest.js
+++ test/dom/FeaturesViewerExclusionTest.js
@@ -22,20 +22,11 @@ var yourDiv = document.getElementById('mocha');
 var FeaturesViewer = require('../..');
 
 describe('FeaturesViewerExclusionTest', function() {
-    var flushAllD3Transitions = function() {
-        var now = Date.now;
-        Date.now = function() { return Infinity; };
-        d3.timer.flush();
-        Date.now = now;
-    };
-
     it('should create 1 category container with 1 category "Domains and Sites"', function(done) {
         var instance = new FeaturesViewer({
             el: yourDiv,
             uniprotacc: 'P05067',
-            display: {
-                domainsAndSites: true
-            }
+            exclusions: ['moleculeProcessing', 'ptm', 'seqInfo', 'structural', 'topology', 'mutagenesis', 'variants']
         });
         instance.getDispatcher().on("ready", function(data) {
             var catContainer = document.querySelectorAll('.up_pftv_container>.up_pftv_category-container');
@@ -55,77 +46,26 @@ describe('FeaturesViewerExclusionTest', function() {
         });
     });
 
-    it('should create only 1 category "Domains and Sites" and dismiss the non-existent one', function(done) {
+    it('should create 2 categories "Topology" and "Mutagenesis", in that order', function(done) {
         var instance = new FeaturesViewer({
             el: yourDiv,
             uniprotacc: 'P05067',
-            display: {
-                domainsAndSites: true,
-                anythingElse: true
-            }
-        });
-        instance.getDispatcher().on("ready", function(data) {
-            var children = document.querySelectorAll('.up_pftv_category-container>.up_pftv_category');
-            assert.equal(children.length, 1, 'category count');
-
-            var catTitle = document.querySelector('.up_pftv_category-container .up_pftv_category-name');
-            assert.equal(catTitle.text.toUpperCase(), "DOMAINS & SITES", 'first category title');
-
-            var categoryFeatures = document.querySelector('.up_pftv_category-container .up_pftv_category-viewer-group');
-            assert.equal(categoryFeatures.childElementCount, data.domainsAndSites.features.length, 'first category' +
-                ' features count');
-            done();
-        });
-    });
-    it('should create only 1 category "Domains and Sites" and dismiss the ones with false as display', function(done) {
-        var instance = new FeaturesViewer({
-            el: yourDiv,
-            uniprotacc: 'P05067',
-            display: {
-                domainsAndSites: true,
-                seqInfo: false,
-                mutagenesis: false
-            }
-        });
-        instance.getDispatcher().on("ready", function(data) {
-            var children = document.querySelectorAll('.up_pftv_category-container>.up_pftv_category');
-            assert.equal(children.length, 1, 'category count');
-
-            var catTitle = document.querySelector('.up_pftv_category-container .up_pftv_category-name');
-            assert.equal(catTitle.text.toUpperCase(), "DOMAINS & SITES", 'first category title');
-
-            var categoryFeatures = document.querySelector('.up_pftv_category-container .up_pftv_category-viewer-group');
-            assert.equal(categoryFeatures.childElementCount, data.domainsAndSites.features.length, 'first category' +
-                ' features count');
-            done();
-        });
-    });
-    it('should create 2 categories "Mutagenesis" and "Topology", in that order, dismissing anything else', function(done) {
-        var instance = new FeaturesViewer({
-            el: yourDiv,
-            uniprotacc: 'P05067',
-            display: {
-                seqInfo: false,
-                mutagenesis: true,
-                anythingElse: true,
-                topology: true,
-                anyOther: false
-            }
+            exclusions: ['domainsAndSites', 'moleculeProcessing', 'ptm', 'seqInfo', 'structural', 'variants']
         });
         instance.getDispatcher().on("ready", function(data) {
             var children = document.querySelectorAll('.up_pftv_category-container>.up_pftv_category');
             assert.equal(children.length, 2, 'category count');
 
             var catTitle = document.querySelectorAll('.up_pftv_category-container .up_pftv_category-name');
-            assert.equal(catTitle[0].text.toUpperCase(), "MUTAGENESIS", 'first category title');
-            assert.equal(catTitle[1].text.toUpperCase(), "TOPOLOGY", 'second category title');
+            assert.equal(catTitle[0].text.toUpperCase(), "TOPOLOGY", 'second category title');
+            assert.equal(catTitle[1].text.toUpperCase(), "MUTAGENESIS", 'first category title');
 
             var categoryFeatures = document.querySelectorAll('.up_pftv_category-container' +
                 ' .up_pftv_category-viewer-group');
             assert.equal(categoryFeatures.length, 5, 'category and type tracks number');
-            assert.equal(categoryFeatures[0].childElementCount, data.mutagenesis.features.length, 'first category' +
+            assert.equal(categoryFeatures[0].childElementCount, data.topology.features.length, 'second category' +
                 ' features count');
-            assert.equal(categoryFeatures[2].childElementCount, data.topology.features.length, 'second category' +
+            assert.equal(categoryFeatures[3].childElementCount, data.mutagenesis.features.length, 'first category' +
                 ' features count');
             done();
         });
