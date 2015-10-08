8cd7907867ec268a7f87678c35c0381430e46d5b
ljgarcia <leylajael@gmail.com>
2015-10-08 11:28:47 +0100

    Add (de)select public methods to FTViewer.

--- snippets/simple_example.js
+++ snippets/simple_example.js
@@ -2,13 +2,13 @@
 var app = require("biojs-vis-proteinfeaturesviewer");
 var instance = new app({el: yourDiv, text: 'biojs', uniprotacc : 'P05067'});
 instance.getDispatcher().on("ready", function() {
- console.log('ready');
+    console.log('ready');
 });
 instance.getDispatcher().on("featureSelected", function(obj) {
- console.log('feature selected');
- console.log(obj);
+    console.log('feature selected');
+    console.log(obj);
 });
 instance.getDispatcher().on("featureDeselected", function(obj) {
- console.log('feature deselected');
- console.log(obj);
+    console.log('feature deselected');
+    console.log(obj);
 });
