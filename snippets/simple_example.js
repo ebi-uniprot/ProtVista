bc2cdb3907117af4225ac665c6f52f36a7177c1b
Xavier Watkins <xavier.watkins@gmail.com>
2015-11-25 15:40:35 +0000

    Added 2 separate examples for pinpad or not

--- snippets/simple_example.js
+++ snippets/simple_example.js
@@ -1,6 +1,6 @@
 // if you don't specify a html file, the sniper will generate a div
 var app = require("biojs-vis-proteinfeaturesviewer");
-var instance = new app({el: yourDiv, text: 'biojs', uniprotacc : 'P05067', pinPad: true});
+var instance = new app({el: yourDiv, text: 'biojs', uniprotacc : 'P05067'});
 instance.getDispatcher().on("ready", function() {
     console.log('ready');
 });
---
+++ snippets/with_pinpad.js
@@ -0,0 +1,3 @@
+// if you don't specify a html file, the sniper will generate a div
+var app = require("biojs-vis-proteinfeaturesviewer");
+var instance = new app({el: yourDiv, text: 'biojs', uniprotacc : 'P05067', pinPad: true});
