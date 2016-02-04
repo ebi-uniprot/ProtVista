fdc4957f8ec8e19867c4e8926876ecc5ac585eb5
ljgarcia <leylajael@gmail.com>
2016-02-04 16:59:53 +0000

    Support variant selection.

--- snippets/simple_example.js
+++ snippets/simple_example.js
@@ -20,17 +20,15 @@ input.append('span').text('Begin: ');
 input.append('input').attr('type', 'text').attr('id', 'ftBegin');
 input.append('span').text('End: ');
 input.append('input').attr('type', 'text').attr('id', 'ftEnd');
+input.append('span').text('AltSeq: ');
+input.append('input').attr('type', 'text').attr('id', 'atlSeq');
 input.append('button').text('Select')
     .on('click', function() {
+        var altSeq = d3.select('#atlSeq').node().value.toUpperCase();
+        altSeq = altSeq.length === 0 ? undefined : altSeq;
         instance.selectFeature(
-            d3.select('#ftType').node().value,
+            d3.select('#ftType').node().value.toUpperCase(),
             d3.select('#ftBegin').node().value,
-            d3.select('#ftEnd').node().value);
-    });
-input.append('button').text('Deselect')
-    .on('click', function() {
-        instance.deselectFeature(
-            d3.select('#ftType').node().value,
-            d3.select('#ftBegin').node().value,
-            d3.select('#ftEnd').node().value);
+            d3.select('#ftEnd').node().value,
+            altSeq);
     });
