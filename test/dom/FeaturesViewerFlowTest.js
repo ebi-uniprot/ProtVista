120c3a84a0637eb0ba87270c422c6a9c7769ef99
ljgarcia <leylajael@gmail.com>
2016-04-25 15:08:39 +0100

    Adjust unit/dom-tests for new data loader.

--- test/dom/FeaturesViewerFlowTest.js
+++ test/dom/FeaturesViewerFlowTest.js
@@ -8,6 +8,7 @@
 
 // chai is an assertion library
 var chai = require('chai');
+var sinon = require('sinon');
 
 // @see http://chaijs.com/api/assert/
 var assert = chai.assert;
@@ -20,7 +21,8 @@ var yourDiv = document.getElementById('mocha');
 
 // requires your main app (specified in index.js)
 var FeaturesViewer = require('../..');
-var DataLoader = require('../../lib/dataLoader');
+var DataLoader = require('../../src/dataLoader');
+var Constants = require('../../src/Constants');
 
 var verifyShadowAttributes = function(containerClass, path, exactPath, translate, height, x) {
     var categoryShadow = document.querySelector('.' + containerClass + ' .up_pftv_shadow');
@@ -68,8 +70,7 @@ var verifyViewPortAttributes = function(verifyInitialX, verifyFullWidth, fullPat
 };
 
 describe('FeaturesViewerFlowTest', function() {
-    var instance,
-        data, aaWidth, gapRegion,
+    var instance, data, aaWidth, gapRegion,
         firstMetalPosition = 9;
 
     var flushAllD3Transitions = function() {
@@ -80,41 +81,43 @@ describe('FeaturesViewerFlowTest', function() {
     };
 
     before(function(done) {
+        sinon.stub(Constants, 'getDataSources', function() {
+            return [{
+                url: 'https://wwwdev.ebi.ac.uk/uniprot/services/restful/features/',
+                type: 'basic'
+            }];
+        });
         var opts = {
             el: yourDiv,
-            uniprotacc: 'nothing'
+            uniprotacc: 'P05067'
         };
         instance = new FeaturesViewer(opts);
-
-        instance.getDispatcher().on("noData", function() {
-            data = require('../../snippets/data/features.json');
-            data.variants.features = DataLoader.processVariants(data);
-            data = DataLoader.processData(data);
-
-            instance.init(opts, data);
-            instance.data = data;
-        });
 
         instance.getDispatcher().on("ready", function() {
             aaWidth = instance.xScale(2) - instance.xScale(1);
             gapRegion = aaWidth/2;
+            data = instance.data;
             done();
         });
     });
 
+    after(function() {
+        Constants.getDataSources.restore();
+    });
+
     describe('Viewer initialization.', function() {
-        it('should create 1 up_pftv_container with 5 children', function() {
+        it('should create 1 up_pftv_container with 3 children', function() {
             var mochaDiv = document.getElementById('mocha');
             assert.equal(mochaDiv.firstElementChild.getAttribute('class'), 'up_pftv_container', 'up_pftv_container' +
                 ' existence inside test div');
             assert.equal(mochaDiv.childElementCount, 1, 'test div children count');
             var mainContainer = document.getElementsByClassName('up_pftv_container');
             assert.equal(mainContainer.length, 1, 'only one up_pftv_container');
-            assert.equal(mainContainer[0].childElementCount, 5, 'up_pftv_container children count');
+            assert.equal(mainContainer[0].childElementCount, 3, 'up_pftv_container children count');
         });
 
         it('should create 1 nav-ruler with 1 SVG', function() {
-            var navRuler = document.querySelectorAll('.up_pftv_container>.up_pftv_navruler');
+            var navRuler = document.querySelectorAll('.up_pftv_container .up_pftv_navruler');
             assert.equal(navRuler.length, 1, 'only one up_pftv_navruler');
             assert.equal(navRuler[0].childElementCount, 1, 'up_pftv_navruler children count');
 
@@ -138,7 +141,7 @@ describe('FeaturesViewerFlowTest', function() {
         });
 
         it('should create 1 up_pftv_buttons with 4 children', function() {
-            var buttonsDiv = document.querySelectorAll('.up_pftv_container>.up_pftv_buttons');
+            var buttonsDiv = document.querySelectorAll('.up_pftv_container .up_pftv_buttons');
             assert.equal(buttonsDiv.length, 1, 'only one up_pftv_buttons');
             assert.equal(buttonsDiv[0].childElementCount, 4, 'up_pftv_buttons children count');
 
@@ -159,37 +162,38 @@ describe('FeaturesViewerFlowTest', function() {
             assert.equal(aaText.length, instance.sequence.length * 2, 'aa sequence length');
         });
 
-        it('should create 1 category container with 8 categories', function() {
-            var catContainer = document.querySelectorAll('.up_pftv_container>.up_pftv_category-container');
+        it('should create 1 category container with 1 subcontainers', function() {
+            var catContainer = document.querySelectorAll('.up_pftv_container .up_pftv_category-container');
             assert.equal(catContainer.length, 1, 'only one up_pftv_category-container');
-            assert.equal(catContainer[0].childElementCount, 8, 'up_pftv_category-container children count');
+            assert.equal(catContainer[0].childElementCount, 1, 'up_pftv_category-container children count');
 
-            var children = document.querySelectorAll('.up_pftv_category-container>.up_pftv_category');
-            assert.equal(children.length, 8, 'category count');
-
+            var children = document.querySelectorAll('.up_pftv_container .up_pftv_category');
+            assert.equal(children.length, 7, 'category count');
         });
 
-        it('should create 1 category container with 8 category and type tracks', function() {
+        it('should create 1 category container with 7 category and type tracks', function() {
             var categoryFeatures = document.querySelectorAll('.up_pftv_category-container' +
                 ' .up_pftv_category-viewer-group');
             assert.equal(categoryFeatures.length, 27, 'category and type tracks number, variants excluded');
-            assert.equal(categoryFeatures[0].childElementCount, data.domainsAndSites.features.length, 'first category' +
-                ' features count');
-            assert.equal(categoryFeatures[6].childElementCount, data.moleculeProcessing.features.length, 'second category' +
-                ' features count');
-            assert.equal(categoryFeatures[10].childElementCount, data.ptm.features.length, 'third' +
+            assert.equal(categoryFeatures[0].childElementCount, data[0][1].length, 'first' +
                 ' category' +
                 ' features count');
-            assert.equal(categoryFeatures[15].childElementCount, data.seqInfo.features.length, 'fourth' +
+            assert.equal(categoryFeatures[6].childElementCount, data[1][1].length, 'second' +
                 ' category' +
                 ' features count');
-            assert.equal(categoryFeatures[18].childElementCount, data.structural.features.length, 'fifth' +
+            assert.equal(categoryFeatures[10].childElementCount, data[2][1].length, 'third' +
                 ' category' +
                 ' features count');
-            assert.equal(categoryFeatures[22].childElementCount, data.topology.features.length, 'sixth' +
+            assert.equal(categoryFeatures[15].childElementCount, data[3][1].length, 'fourth' +
                 ' category' +
                 ' features count');
-            assert.equal(categoryFeatures[25].childElementCount, data.mutagenesis.features.length, 'seventh' +
+            assert.equal(categoryFeatures[18].childElementCount, data[4][1].length, 'fifth' +
+                ' category' +
+                ' features count');
+            assert.equal(categoryFeatures[22].childElementCount, data[5][1].length, 'sixth' +
+                ' category' +
+                ' features count');
+            assert.equal(categoryFeatures[25].childElementCount, data[6][1].length, 'seventh' +
                 ' category' +
                 ' features count');
         });
@@ -200,7 +204,7 @@ describe('FeaturesViewerFlowTest', function() {
             expect(catTitle.getAttribute('class').indexOf('up_pftv_arrow-right')).to.not.equal(-1);
 
             var categoryFeatures = document.querySelector('.up_pftv_category-container .up_pftv_category-viewer-group');
-            assert.equal(categoryFeatures.childElementCount, data.domainsAndSites.features.length, 'first category' +
+            assert.equal(categoryFeatures.childElementCount, data[0][1].length, 'first category' +
                 ' features count');
         });
 
@@ -225,7 +229,7 @@ describe('FeaturesViewerFlowTest', function() {
         });
 
         it('should create a metal in position 147', function() {
-            var feature = data.domainsAndSites.features[firstMetalPosition];
+            var feature = data[0][1][firstMetalPosition];
             var path = document.querySelector("[name='" + feature.internalId + "']");
             assert.equal(path.getAttribute('d'), 'M0,0L5,5L0,10L-5,5Z', 'metal path');
             assert.equal(path.getAttribute('class'), 'up_pftv_feature up_pftv_metal', 'metal class');
@@ -237,7 +241,7 @@ describe('FeaturesViewerFlowTest', function() {
     describe('Feature selection', function() {
         it('should select a category feature @147', function() {
             instance.overFeature = true;
-            var feature = data.domainsAndSites.features[firstMetalPosition];
+            var feature = data[0][1][firstMetalPosition];
             var paths = document.querySelectorAll("[name='" + feature.internalId + "']");
             assert.equal(paths.length, 2, 'number of metals in position 147 (1 in category, 1 in type)');
             var evt = document.createEvent("MouseEvents");
@@ -251,7 +255,7 @@ describe('FeaturesViewerFlowTest', function() {
         });
 
         it('should activate vertical highlight after feature selection @147', function() {
-            var feature = data.domainsAndSites.features[firstMetalPosition];
+            var feature = data[0][1][firstMetalPosition];
             var paths = document.querySelectorAll("[name='" + feature.internalId + "']");
             var svg = document.querySelector('.up_pftv_category svg');
 
@@ -263,7 +267,7 @@ describe('FeaturesViewerFlowTest', function() {
             var typeShadow = document.querySelector('.up_pftv_track g');
             assert.equal(typeShadow.childElementCount, 1, 'only one vertical shadow');
 
-            var feature = data.domainsAndSites.features[firstMetalPosition];
+            var feature = data[0][1][firstMetalPosition];
             var paths = document.querySelectorAll("[name='" + feature.internalId + "']");
 
             verifyShadowAttributes('up_pftv_track', paths[0], undefined, undefined,
@@ -291,7 +295,7 @@ describe('FeaturesViewerFlowTest', function() {
 
     describe('Feature deselection', function() {
         it('should deselect the selected feature @147', function() {
-            var feature = data.domainsAndSites.features[firstMetalPosition];
+            var feature = data[0][1][firstMetalPosition];
             var paths = document.querySelectorAll("[name='" + feature.internalId + "']");
             var evt = document.createEvent("MouseEvents");
             evt.initMouseEvent("click", true, true, window, 1, 1, 1, 1, 1, false, false, false, false, 0, paths[0]);
@@ -336,7 +340,7 @@ describe('FeaturesViewerFlowTest', function() {
     describe('Type feature selection', function() {
         it('should select a type feature @147', function() {
             instance.overFeature = true;
-            var feature = data.domainsAndSites.features[firstMetalPosition];
+            var feature = data[0][1][firstMetalPosition];
             var paths = document.querySelectorAll("[name='" + feature.internalId + "']");
             assert.equal(paths.length, 2, 'number of metals in position 147 (1 in category, 1 in type)');
             var evt = document.createEvent("MouseEvents");
@@ -352,7 +356,7 @@ describe('FeaturesViewerFlowTest', function() {
         });
 
         it('should activate all vertical highlight after feature selection @147', function() {
-            var feature = data.domainsAndSites.features[firstMetalPosition];
+            var feature = data[0][1][firstMetalPosition];
             var paths = document.querySelectorAll("[name='" + feature.internalId + "']");
 
             var transform = paths[0].getAttribute('transform');
@@ -382,7 +386,7 @@ describe('FeaturesViewerFlowTest', function() {
         });
 
         it('should keep selection on selected feature after closing', function() {
-            var feature = data.domainsAndSites.features[firstMetalPosition];
+            var feature = data[0][1][firstMetalPosition];
             var paths = document.querySelectorAll("[name='" + feature.internalId + "']");
 
             assert.equal(paths[0].getAttribute('class'), 'up_pftv_feature up_pftv_metal up_pftv_activeFeature'
@@ -396,10 +400,10 @@ describe('FeaturesViewerFlowTest', function() {
     describe('Feature selection when another on is already selected', function() {
         it('should select another feature, first molecule processing feature @1-17', function() {
             instance.overFeature = true;
-            var featureDS = data.domainsAndSites.features[firstMetalPosition];
+            var featureDS = data[0][1][firstMetalPosition];
             var pathsDS = document.querySelectorAll("[name='" + featureDS.internalId + "']");
 
-            var featureMP = data.moleculeProcessing.features[0];
+            var featureMP = data[1][1][0];
             var pathsMP = document.querySelectorAll("[name='" + featureMP.internalId + "']");
             assert.equal(pathsMP[0].getAttribute('class'), 'up_pftv_feature up_pftv_signal', 'non selected signal class');
             var evtMP = document.createEvent("MouseEvents");
@@ -415,7 +419,7 @@ describe('FeaturesViewerFlowTest', function() {
         });
 
         it('should activate vertical highlight after feature selection @1-17', function() {
-            var featureMP = data.moleculeProcessing.features[0];
+            var featureMP = data[1][1][0];
             var paths = document.querySelectorAll("[name='" + featureMP.internalId + "']");
 
             var svg = document.querySelector('.up_pftv_category svg');
@@ -428,7 +432,7 @@ describe('FeaturesViewerFlowTest', function() {
         });
 
         it('should adjust vertical highlight for type tracks after feature selection @1-17', function() {
-            var featureMP = data.moleculeProcessing.features[0];
+            var featureMP = data[1][1][0];
             var paths = document.querySelectorAll("[name='" + featureMP.internalId + "']");
 
             var shapePath = paths[0].getAttribute('d');
@@ -447,7 +451,7 @@ describe('FeaturesViewerFlowTest', function() {
             zoomIn.dispatchEvent(evt); //zoom in
             flushAllD3Transitions();
 
-            var feature = data.domainsAndSites.features[firstMetalPosition];
+            var feature = data[0][1][firstMetalPosition];
             var path = document.querySelector("[name='" + feature.internalId + "']");
             assert.equal(path.getAttribute('transform'), 'translate(' + instance.xScale(+feature.begin) + ',5)'
                 , 'translated metal');
@@ -464,7 +468,7 @@ describe('FeaturesViewerFlowTest', function() {
         });
 
         it('should adjust vertical highlight after zooming', function() {
-            var featureMP = data.moleculeProcessing.features[0];
+            var featureMP = data[1][1][0];
             var paths = document.querySelectorAll("[name='" + featureMP.internalId + "']");
 
             var shapePath = paths[0].getAttribute('d');
@@ -493,7 +497,7 @@ describe('FeaturesViewerFlowTest', function() {
         });
 
         it('should keep shadow after zooming-out', function() {
-            var featureMP = data.moleculeProcessing.features[0];
+            var featureMP = data[1][1][0];
             var paths = document.querySelectorAll("[name='" + featureMP.internalId + "']");
 
             var shapePath = paths[0].getAttribute('d');
@@ -515,7 +519,7 @@ describe('FeaturesViewerFlowTest', function() {
             assert.equal(selectedFeature.length, 1, 'feature still selected');
             expect(instance.selectedFeature).to.be.not.undefined;
 
-            var featureMP = data.moleculeProcessing.features[0];
+            var featureMP = data[1][1][0];
             var paths = document.querySelectorAll("[name='" + featureMP.internalId + "']");
 
             var shapePath = paths[0].getAttribute('d');
@@ -571,7 +575,7 @@ describe('FeaturesViewerFlowTest', function() {
 
     describe('Deselection on click outside the FT', function() {
         it('should deselect feature on svg click', function() {
-            var featureMP = data.moleculeProcessing.features[0];
+            var featureMP = data[1][1][0];
             var pathsMP = document.querySelectorAll("[name='" + featureMP.internalId + "']");
             var evtMP = document.createEvent("MouseEvents");
             evtMP.initMouseEvent("click", true, true, window, 1, 1, 1, 1, 1, false, false, false, false, 0, pathsMP[0]);
@@ -610,13 +614,13 @@ describe('FeaturesViewerFlowTest', function() {
             assert.equal(tooltip.length, 1, 'region tooltip exists');
         });
 
-        it('should select a VARIANT feature', function() {
-            instance.selectFeature('VARIANT', 23, 23, 'G');
+        it('should select a MUTAGENESIS feature', function() {
+            instance.selectFeature('MUTAGEN', 198, 198, 'A');
             var activeFeature = document.querySelector('.up_pftv_activeFeature');
 
-            assert.equal(instance.selectedFeature.begin, 23, 'begin selected variant feature');
-            assert.equal(instance.selectedFeature.end, 23, 'end selected variant feature');
-            assert.equal(activeFeature.getAttribute('class'), 'up_pftv_variant up_pftv_activeFeature',
+            assert.equal(instance.selectedFeature.begin, 198, 'begin selected variant feature');
+            assert.equal(instance.selectedFeature.end, 198, 'end selected variant feature');
+            assert.equal(activeFeature.getAttribute('class'), 'up_pftv_feature up_pftv_mutagen up_pftv_activeFeature',
                 'selected variant class');
         });
 
