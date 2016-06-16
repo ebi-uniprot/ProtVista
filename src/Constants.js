34adc70c8db60a3e6977bc7d2058752a88cb3962
ljgarcia <leylajael@gmail.com>
2016-06-16 17:59:18 +0100

    Add methods to add categories and tracks.

--- src/Constants.js
+++ src/Constants.js
@@ -4,6 +4,118 @@
 
 var _ = require('underscore');
 
+var basicType = 'basic';
+var variantType = 'variant';
+
+var sources = [
+    {
+        url: 'https://www.ebi.ac.uk/uniprot/api/features/',
+        type: basicType,
+        authority: 'uniprot'
+    }, {
+        url: 'https://www.ebi.ac.uk/uniprot/api/proteomics/',
+        type: basicType,
+        authority: 'uniprot'
+    }, {
+        url: 'https://www.ebi.ac.uk/uniprot/api/variation/',
+        type: variantType,
+        authority: 'uniprot'
+    }
+];
+
+var categories = [
+    {
+        type: basicType,
+        categoryNamesInOrder: [
+            {DOMAINS_AND_SITES: 'Domains & sites'},
+            {MOLECULE_PROCESSING: 'Molecule processing'},
+            {PTM: 'Post translational modifications'},
+            {SEQUENCE_INFORMATON: 'Sequence information'},
+            {STRUCTURAL: 'Structural features'},
+            {TOPOLOGY: 'Topology'},
+            {MUTAGENESIS: 'Mutagenesis'},
+            {PROTEOMICS: 'Proteomics'}
+        ]
+    }, {
+        type: variantType,
+        categoryNamesInOrder: [
+            {VARIATION: 'Variants'}
+        ]
+    }
+];
+
+var trackNames = {
+    //molecular processing
+    chain: {label: 'Chain',
+        tooltip: '(aka mature region). This describes the extent of a polypeptide chain in the mature protein' +
+        ' following processing'
+    }, transit: {label: 'Transit peptide', tooltip: 'This describes the extent of a transit peptide'
+    }, init_met: {label: 'Initiator methionine', tooltip: 'This indicates that the initiator methionine is cleaved from the mature' +
+    ' protein'
+    }, propep: {label: 'Propeptide', tooltip: 'Part of a protein that is cleaved during maturation or activation'
+    }, peptide: {label: 'Peptide', tooltip: 'The position and length of an active peptide in the mature protein'
+    }, signal: {label: 'Signal', tooltip: 'N-terminal signal peptide'},
+    //structural
+    helix: {label: 'Helix', tooltip: 'The positions of experimentally determined helical regions'
+    }, strand: {label: 'Beta strand', tooltip: 'The positions of experimentally determined beta strands'
+    }, turn: {label: 'Turn', tooltip: 'The positions of experimentally determined hydrogen-bonded turns'
+    }, disulfid: {label: 'Disulfide bond', tooltip: 'The positions of cysteine residues participating in' +
+    ' disulphide bonds'
+    }, crosslnk: {label: 'Cross-link', tooltip: 'Covalent linkages of various types formed between two proteins' +
+    ' or between two parts of the same protein'},
+    //domains & sites
+    region: {label: 'Region', tooltip: 'Regions in multifunctional enzymes or fusion proteins, or' +
+    ' characteristics of a region, e.g., protein-protein interactions mediation'
+    }, coiled:  {label: 'Coiled coil', tooltip: 'Coiled coils are built by two or more alpha-helices that wind' +
+    ' around each other to form a supercoil'
+    }, motif: {label: 'Motif', tooltip: 'Short conserved sequence motif of biological significance'
+    }, repeat: {label: 'Repeat', tooltip: 'Repeated sequence motifs or repeated domains within the protein'
+    }, ca_bind: {label: 'Calcium binding', tooltip: 'Calcium-binding regions, such as the EF-hand motif'
+    }, dna_bind: {label: 'DNA binding', tooltip: 'DNA-binding domains such as AP2/ERF domain, the ETS domain,' +
+    ' the Fork-Head domain, the HMG box and the Myb domain'
+    }, domain: {label: 'Domain', tooltip: 'Specific combination of secondary structures organized into a ' +
+    'characteristic three-dimensional structure or fold'
+    }, zn_fing: {label: 'Zinc finger', tooltip: 'Small, functional, independently folded domain that coordinates' +
+    ' one or more zinc ions'
+    }, np_bind: {label: 'Nucleotide binding', tooltip: '(aka flavin-binding). Region in the protein which binds' +
+    ' nucleotide phosphates'
+    }, metal: {label: 'Metal binding', tooltip: 'Binding site for a metal ion'
+    }, site: {label: 'Site', tooltip: 'Any interesting single amino acid site on the sequence'
+    }, binding: {label: 'Binding site', tooltip: 'Binding site for any chemical group (co-enzyme, prosthetic' +
+    ' group, etc.)'
+    }, act_site: {label: 'Active site', tooltip: 'Amino acid(s) directly involved in the activity of an enzyme'},
+    //ptms
+    mod_res: {label: 'Modified residue', tooltip: 'Modified residues such as phosphorylation, acetylation,' +
+    ' acylation, methylation'
+    }, lipid: {label: 'Lipidation', tooltip: 'Covalently attached lipid group(s)'
+    }, carbohyd: {label: 'Glycosylation', tooltip: 'Covalently attached glycan group(s)'},
+    //seqInfo
+    compbias: {label: 'Compositional bias', tooltip: 'Position of regions of compositional bias within the' +
+    ' protein and the particular amino acids that are over-represented within those regions'
+    }, conflict: {label: 'Sequence conflict', tooltip: 'Sequence discrepancies of unknown origin'
+    }, non_cons: {label: 'Non-adjacent residues', tooltip: 'Indicates that two residues in a sequence are not ' +
+    'consecutive and that there is an undetermined number of unsequenced residues between them'
+    }, non_ter: {label: 'Non-terminal residue', tooltip: 'The sequence is incomplete. The residue is not the ' +
+    'terminal residue of the complete protein'
+    }, unsure: {label: 'Sequence uncertainty', tooltip: 'Regions of a sequence for which the authors are unsure ' +
+    'about the sequence assignment'
+    }, non_std: {label: 'Non-standard residue', tooltip: 'Non-standard amino acids (selenocysteine and ' +
+    'pyrrolysine)'},
+    //mutagenesis
+    mutagen: {label: 'Mutagenesis', tooltip: 'Site which has been experimentally altered by mutagenesis'},
+    //topology
+    topo_dom: {label: 'Topological domain', tooltip: 'Location of non-membrane regions of membrane-spanning ' +
+    'proteins'
+    }, transmem: {label: 'Transmembrane', tooltip: 'Extent of a membrane-spanning region'
+    }, intramem: {label: 'Intramembrane', tooltip: 'Extent of a region located in a membrane without crossing it'},
+    //variants
+    variant: {label: 'Natural variant', tooltip: 'Natural variant of the protein, including polymorphisms, ' +
+    'variations between strains, isolates or cultivars, disease-associated mutations and RNA editing events'
+    },
+    unique:{label:'Unique peptide',tooltip:''},
+    non_unique:{label:'Non-unique peptide',tooltip:''}
+};
+
 var Constants = function() {
   return {
     getBlastURL: function() {
@@ -13,38 +125,36 @@ var Constants = function() {
       return ['helix', 'strand', 'turn', 'disulfid', 'crosslnk', 'variant'];
     },
     getDataSources: function() {
-      var sources = [{
-        url: 'https://www.ebi.ac.uk/uniprot/api/features/',
-        type: 'basic'
-      }, {
-        url: 'https://www.ebi.ac.uk/uniprot/api/proteomics/',
-        type: 'basic'
-      }, {
-        url: 'https://www.ebi.ac.uk/uniprot/api/variation/',
-        type: 'variant'
-      }];
       return sources;
-    }, getCategoryNamesInOrder: function() {
-      return [{
-        DOMAINS_AND_SITES: 'Domains & sites'
-      }, {
-        MOLECULE_PROCESSING: 'Molecule processing'
-      }, {
-        PTM: 'Post translational modifications'
-      }, {
-        SEQUENCE_INFORMATON: 'Sequence information'
-      }, {
-        STRUCTURAL: 'Structural features'
-      }, {
-        TOPOLOGY: 'Topology'
-      }, {
-        MUTAGENESIS: 'Mutagenesis'
-      }, {
-        PROTEOMICS: 'Proteomics'
-      }, {
-        VARIATION: 'Variants'
-      }];
-    }, getCategoryName: function(name) {
+    },
+    addSource: function(source) {
+        if (!source.type) {
+            source.type = 'basic';
+        }
+        sources.push(source);
+    },
+    getCategoryNamesInOrder: function() {
+        var temp = [];
+        _.each(categories, function(cat) {
+            temp = _.union(temp, cat.categoryNamesInOrder);
+        });
+        return temp;
+    },
+    addCategories: function(source, categoryNamesInOrder) {
+        var typeGroup = _.find(categories, function(cat) {return cat.type === source.type});
+        if (typeGroup) {
+            var keys = [];
+            _.each(typeGroup.categoryNamesInOrder, function(elem) {
+                keys.push(_.keys(elem)[0]);
+            });
+            _.each(categoryNamesInOrder, function(elem) {
+                if (!_.contains(keys, _.keys(elem)[0])) {
+                    typeGroup.categoryNamesInOrder.push(elem);
+                }
+            });
+        }
+    },
+    getCategoryName: function(name) {
       var names = this.getCategoryNamesInOrder();
       var match = _.find( names, function(item){
         return name === _.keys(item)[0];
@@ -55,79 +165,17 @@ var Constants = function() {
         return name;
       }
     }, getTrackNames: function() {
-      return {
-          //molecular processing
-          chain: {label: 'Chain',
-              tooltip: '(aka mature region). This describes the extent of a polypeptide chain in the mature protein' +
-              ' following processing'
-          }, transit: {label: 'Transit peptide', tooltip: 'This describes the extent of a transit peptide'
-          }, init_met: {label: 'Initiator methionine', tooltip: 'This indicates that the initiator methionine is cleaved from the mature' +
-              ' protein'
-          }, propep: {label: 'Propeptide', tooltip: 'Part of a protein that is cleaved during maturation or activation'
-          }, peptide: {label: 'Peptide', tooltip: 'The position and length of an active peptide in the mature protein'
-          }, signal: {label: 'Signal', tooltip: 'N-terminal signal peptide'},
-          //structural
-          helix: {label: 'Helix', tooltip: 'The positions of experimentally determined helical regions'
-          }, strand: {label: 'Beta strand', tooltip: 'The positions of experimentally determined beta strands'
-          }, turn: {label: 'Turn', tooltip: 'The positions of experimentally determined hydrogen-bonded turns'
-          }, disulfid: {label: 'Disulfide bond', tooltip: 'The positions of cysteine residues participating in' +
-              ' disulphide bonds'
-          }, crosslnk: {label: 'Cross-link', tooltip: 'Covalent linkages of various types formed between two proteins' +
-              ' or between two parts of the same protein'},
-          //domains & sites
-          region: {label: 'Region', tooltip: 'Regions in multifunctional enzymes or fusion proteins, or' +
-              ' characteristics of a region, e.g., protein-protein interactions mediation'
-          }, coiled:  {label: 'Coiled coil', tooltip: 'Coiled coils are built by two or more alpha-helices that wind' +
-              ' around each other to form a supercoil'
-          }, motif: {label: 'Motif', tooltip: 'Short conserved sequence motif of biological significance'
-          }, repeat: {label: 'Repeat', tooltip: 'Repeated sequence motifs or repeated domains within the protein'
-          }, ca_bind: {label: 'Calcium binding', tooltip: 'Calcium-binding regions, such as the EF-hand motif'
-          }, dna_bind: {label: 'DNA binding', tooltip: 'DNA-binding domains such as AP2/ERF domain, the ETS domain,' +
-              ' the Fork-Head domain, the HMG box and the Myb domain'
-          }, domain: {label: 'Domain', tooltip: 'Specific combination of secondary structures organized into a ' +
-              'characteristic three-dimensional structure or fold'
-          }, zn_fing: {label: 'Zinc finger', tooltip: 'Small, functional, independently folded domain that coordinates' +
-              ' one or more zinc ions'
-          }, np_bind: {label: 'Nucleotide binding', tooltip: '(aka flavin-binding). Region in the protein which binds' +
-              ' nucleotide phosphates'
-          }, metal: {label: 'Metal binding', tooltip: 'Binding site for a metal ion'
-          }, site: {label: 'Site', tooltip: 'Any interesting single amino acid site on the sequence'
-          }, binding: {label: 'Binding site', tooltip: 'Binding site for any chemical group (co-enzyme, prosthetic' +
-          ' group, etc.)'
-          }, act_site: {label: 'Active site', tooltip: 'Amino acid(s) directly involved in the activity of an enzyme'},
-          //ptms
-          mod_res: {label: 'Modified residue', tooltip: 'Modified residues such as phosphorylation, acetylation,' +
-              ' acylation, methylation'
-          }, lipid: {label: 'Lipidation', tooltip: 'Covalently attached lipid group(s)'
-          }, carbohyd: {label: 'Glycosylation', tooltip: 'Covalently attached glycan group(s)'},
-          //seqInfo
-          compbias: {label: 'Compositional bias', tooltip: 'Position of regions of compositional bias within the' +
-              ' protein and the particular amino acids that are over-represented within those regions'
-          }, conflict: {label: 'Sequence conflict', tooltip: 'Sequence discrepancies of unknown origin'
-          }, non_cons: {label: 'Non-adjacent residues', tooltip: 'Indicates that two residues in a sequence are not ' +
-              'consecutive and that there is an undetermined number of unsequenced residues between them'
-          }, non_ter: {label: 'Non-terminal residue', tooltip: 'The sequence is incomplete. The residue is not the ' +
-              'terminal residue of the complete protein'
-          }, unsure: {label: 'Sequence uncertainty', tooltip: 'Regions of a sequence for which the authors are unsure ' +
-              'about the sequence assignment'
-          }, non_std: {label: 'Non-standard residue', tooltip: 'Non-standard amino acids (selenocysteine and ' +
-              'pyrrolysine)'},
-          //mutagenesis
-          mutagen: {label: 'Mutagenesis', tooltip: 'Site which has been experimentally altered by mutagenesis'},
-          //topology
-          topo_dom: {label: 'Topological domain', tooltip: 'Location of non-membrane regions of membrane-spanning ' +
-              'proteins'
-          }, transmem: {label: 'Transmembrane', tooltip: 'Extent of a membrane-spanning region'
-          }, intramem: {label: 'Intramembrane', tooltip: 'Extent of a region located in a membrane without crossing it'},
-          //variants
-          variant: {label: 'Natural variant', tooltip: 'Natural variant of the protein, including polymorphisms, ' +
-              'variations between strains, isolates or cultivars, disease-associated mutations and RNA editing events'
-          },
-          unique:{label:'Unique peptide',tooltip:''},
-          non_unique:{label:'Non-unique peptide',tooltip:''}
-      };
-    }, getTrackInfo: function(name) {
-      return this.getTrackNames()[name];
+      return trackNames;
+    },
+    addTrackName: function(tracksToAdd) {
+        _.each(tracksToAdd, function(elem, key) {
+            if (!trackNames[key]) {
+                trackNames[key] = elem;
+            }
+        });
+    },
+    getTrackInfo: function(name) {
+      return this.getTrackNames()[name] ? this.getTrackNames()[name] : {label: name, tooltip:''};
     }
   };
 }();
--- test/unit/FeaturesViewerTest.js
+++ test/unit/FeaturesViewerTest.js
@@ -19,61 +19,119 @@ chai.should();
 
 // requires your main app
 var _ = require("underscore");
-var FeaturesViewer = require('../..');
+var Constants = require('../../src/Constants');
 var DataLoader = require('../../src/DataLoader');
 var NonOverlappingLayout = require('../../src/NonOverlappingLayout');
 
 describe('FeaturesViewerTest', function() {
-	//'DataLoader'
-    var data = require('../../snippets/data/features.json');
-    var basicFeatures = DataLoader.groupFeaturesByCategory(data.features);
+    describe('DataLoader', function() {
+        //'DataLoader'
+        var data = require('../../snippets/data/features.json');
+        var basicFeatures = DataLoader.groupFeaturesByCategory(data.features);
 
-	it('should process the data', function(done) {
-		var variants = _.contains(basicFeatures, function(cat) {
-			return (cat[0] === 'VARIANTS') || (cat[0] === 'VARIATION');
-		});
-		assert.equal(variants, false);
+        it('should process the data', function(done) {
+            var variants = _.contains(basicFeatures, function(cat) {
+                return (cat[0] === 'VARIANTS') || (cat[0] === 'VARIATION');
+            });
+            assert.equal(variants, false);
 
-        var ftVariants = _.filter(data.features, function(ft) {
-            return ft.category === 'VARIANTS';
+            var ftVariants = _.filter(data.features, function(ft) {
+                return ft.category === 'VARIANTS';
+            });
+            var totalFeatures = data.features.length - ftVariants.length;
+            var totalCount = 0;
+            _.each(basicFeatures, function(cat) {
+                totalCount += cat[1].length;
+            });
+            assert.equal(totalFeatures, totalCount);
+            done();
         });
-        var totalFeatures = data.features.length - ftVariants.length;
-		var totalCount = 0;
-        _.each(basicFeatures, function(cat) {
-			totalCount += cat[1].length;
-		});
-        assert.equal(totalFeatures, totalCount);
-        done();
-	});
 
-	it('should process variants', function() {
-        var data = require('../../snippets/data/variant.json');
-        var variants = DataLoader.processVariants(data.features, data.sequence);
-        assert.equal(variants[0].length, 2);
-        assert.equal(variants[0][0], 'VARIATION');
-        assert.equal(variants[0][1].length, data.sequence.length + 2);
-	});
+        it('should process variants', function() {
+            var data = require('../../snippets/data/variant.json');
+            var variants = DataLoader.processVariants(data.features, data.sequence);
+            assert.equal(variants[0].length, 2);
+            assert.equal(variants[0][0], 'VARIATION');
+            assert.equal(variants[0][1].length, data.sequence.length + 2);
+        });
 
-    it('should process proteomics', function() {
-        var data = require('../../snippets/data/peptides.json');
-        var proteomics = DataLoader.processProteomics(data.features, data.sequence);
-        assert.equal(proteomics[0].length, 2);
-        assert.equal(proteomics[0][0], 'PROTEOMICS');
-        assert.equal(proteomics[0][1].length, data.features.length);
+        it('should process proteomics', function() {
+            var data = require('../../snippets/data/peptides.json');
+            var proteomics = DataLoader.processProteomics(data.features, data.sequence);
+            assert.equal(proteomics[0].length, 2);
+            assert.equal(proteomics[0][0], 'PROTEOMICS');
+            assert.equal(proteomics[0][1].length, data.features.length);
+        });
+
+        //'NonOverlappingLayout
+        var layout = new NonOverlappingLayout(basicFeatures[0][1], 40);
+
+        it('should calculate track overlaps', function() {
+            layout.calculate();
+        });
+
+        it('should return feature height', function() {
+            assert.equal(13,layout.getFeatureHeight());
+        });
+
+        it('should return right number of rows', function() {
+            assert.equal(2, layout.getRows().length);
+        });
     });
+    describe('Add external sources', function () {
+        var source = {
+            url: 'https://my.url.com',
+            type: 'basic',
+            authority: 'my lab'
+        };
+        it ('should add a new source', function() {
+            var expectedSources = Constants.getDataSources().slice(0);
+            expectedSources.push(source);
+            Constants.addSource(source);
+            assert.equal(JSON.stringify(expectedSources), JSON.stringify(Constants.getDataSources()));
+        });
+        it('should add only new categories', function() {
+            var expectedCategoryNamesInOrder = [
+                {DOMAINS_AND_SITES: 'Domains & sites'}, {MOLECULE_PROCESSING: 'Molecule processing'},
+                {PTM: 'Post translational modifications'}, {SEQUENCE_INFORMATON: 'Sequence information'} ,
+                {STRUCTURAL: 'Structural features'}, { TOPOLOGY: 'Topology'},
+                {MUTAGENESIS: 'Mutagenesis'}, {PROTEOMICS: 'Proteomics'},
+                {MY_DOMAINS: 'My domains'}, {CAT: 'My cat'},
+                {VARIATION: 'Variants'}
+            ];
 
-	//'NonOverlappingLayout
-	var layout = new NonOverlappingLayout(basicFeatures[0][1], 40);
+            var categoriesToAdd = [
+                {MY_DOMAINS: 'My domains'}, {MOLECULE_PROCESSING: 'Molecule processing'}, {CAT: 'My cat'}
+            ];
+            Constants.addCategories(source, categoriesToAdd);
+            assert.equal(JSON.stringify(expectedCategoryNamesInOrder),
+                JSON.stringify(Constants.getCategoryNamesInOrder()));
+        });
+        it('should add only new tracks', function() {
+            var keys = _.keys(Constants.getTrackNames());
+            var howMany = keys.length;
+            var label = Constants.getTrackNames().unique.label;
 
-	it('should calculate track overlaps', function() {
-		layout.calculate();
-	});
+            var tracksToAdd = {
+                only_unique: {label: 'Only unique peptide', tooltip: ''},
+                unique: {label: 'Unique peptide whose track already exist', tooltip: ''},
+                mixed: {label: 'Mixed peptide', tooltip: ''}
+            };
 
-	it('should return feature height', function() {
-		assert.equal(13,layout.getFeatureHeight());
-	});
-
-	it('should return right number of rows', function() {
-		assert.equal(2, layout.getRows().length);
-	});
+            Constants.addTrackName(tracksToAdd);
+            keys = _.keys(Constants.getTrackNames());
+            assert.equal(howMany + 2, keys.length);
+            assert.equal(Constants.getTrackNames().unique.label, label);
+            assert.deepEqual(Constants.getTrackNames().mixed, tracksToAdd.mixed);
+            assert.deepEqual(Constants.getTrackNames().only_unique, tracksToAdd.only_unique);
+        });
+        it('should get existing track info', function() {
+            var unique = {label:'Unique peptide',tooltip:''};
+            assert.deepEqual(unique, Constants.getTrackInfo('unique'));
+        });
+        it('should get non-existing track info', function() {
+            var family_Domains = {label:'family_Domains', tooltip:''};
+            assert.deepEqual(family_Domains, Constants.getTrackInfo('family_Domains'));
+        });
+    });
 });
