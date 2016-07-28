361fe949d4188e699cf9b524aaaa8477a932e396
ljgarcia <leylajael@gmail.com>
2016-07-28 13:55:05 +0100

    Fix bug on deletion variants tooltip.

--- src/TooltipFactory.js
+++ src/TooltipFactory.js
@@ -360,7 +360,9 @@ var addAssociation = function(tooltip) {
 var addMutation = function(tooltip) {
     var mutRow = tooltip.table.append('tr');
     mutRow.append('td').text('Variant');
-    var text = (tooltip.data.wildType === '-' ? tooltip.sequence.charAt(+tooltip.data.begin): tooltip.data.wildType)
+    var text = (tooltip.data.wildType === '-'
+            ? tooltip.sequence.substring(+tooltip.data.begin, +tooltip.data.end+1)
+            : tooltip.data.wildType)
         + ' > ' +
         (tooltip.data.alternativeSequence === '-' ? 'del' : tooltip.data.alternativeSequence);
     mutRow.append('td').text(text);
