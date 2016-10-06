de2df5b02b6f5369190917be6be482b751587078
ljgarcia <leylajael@gmail.com>
2016-10-06 10:53:21 +0100

    Start code reorganization for download.

--- src/DownloadDataLoader.js
+++ src/DownloadDataLoader.js
@@ -11,20 +11,20 @@ var FileSaver = require('file-saver');
     var DownloadDataLoader = function() {
     return {
         get: function(accession, format) {
-            if (format === 'JSON') {
-                this.getJSON(accession);
-            }
-        },
-        getJSON: function(accession) {
             var zip = new JSZip();
-            //zip.file("Hello.txt", "Hello World\n");   //TODO Maybe a readme?
+            zip.file("Hello.txt", "Hello World\n");   //TODO Maybe a readme?
             var loaders = [];
             _.each(Constants.getUniProtDataSources(), function(source) {
                 //TODO decision should we support 3-party data sources download?
                 //TODO decision if 3-party data sources do not support gff/xml, check the response headers and inform
                 //TODO load only categories that are displayed... but that might not work for 3-party sources... we
                 // would still need the filtering code
-                var loader = $.getJSON(source.url + accession);
+                //var loader = $.getJSON(source.url + accession);
+                var extension = '';
+                var loader = $.get({
+                    url: source.url + accession + extension,
+                    dataType: format === 'XML' ? 'xml' : (format === 'JSON') ? 'json' : 'text'
+                });
                 loaders.push(loader);
                 loader.done(function(d) {
                     if (source.category === 'GENERAL') { //TODO get rid of hardcoded here
@@ -32,19 +32,28 @@ var FileSaver = require('file-saver');
                             return feature.category !== 'VARIANTS';
                         });
                     }
-                    zip.file(source.authority + source.category + '.json', JSON.stringify(d));
+                    if (format === 'JSON') {
+                        zip.file(source.authority + source.category + '.json', JSON.stringify(d));
+                    }
+
                 }).fail(function (e) {
-                     console.log('DownloadDataLoader', e);
+                    console.log('DownloadDataLoader', e);
                 });
             });
             //when all done
             $.when.apply(null, loaders).done(function () {
                 zip.generateAsync({type:"blob"})
-                .then(function(content) {
-                    // TODO FileSaver does not necessarily work in Safari
-                    FileSaver.saveAs(content, "data.zip");
-                });
+                    .then(function(content) {
+                        // TODO FileSaver does not necessarily work in Safari
+                        FileSaver.saveAs(content, "data.zip");
+                    });
             });
+            if (format === 'JSON') {
+                this.getJSON(accession);
+            }
+        },
+        getJSON: function(accession) {
+
         },
         getXML: function() {
 
