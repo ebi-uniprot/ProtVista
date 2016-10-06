81eeb71001f15f42f5a52350facf0862a95e4887
ljgarcia <leylajael@gmail.com>
2016-10-06 11:23:14 +0100

    Create VariantCategoryViewer module.

--- src/CategoryFactory.js
+++ src/CategoryFactory.js
@@ -12,6 +12,7 @@ var BasicViewer = require("./BasicViewer");
 var ViewerHelper = require("./ViewerHelper");
 var Constants = require("./Constants");
 var Evidence = require('./Evidence');
+var VariantCategoryViewer = require('./VariantCategoryViewer');
 
 var Category = function(name, data, catInfo, fv, container) {
     var category = this;
@@ -164,85 +165,6 @@ var BasicCategoryViewer = function(category) {
     return new BasicViewer(
         category.name, category.data, category.viewerContainer, category.fv
     );
-};
-
-var VariantCategoryViewer = function(category) {
-    var varCatViewer = this;
-    varCatViewer.features = category.data;
-
-    var height = 40;
-    var container = category.viewerContainer,
-        xScale = category.fv.xScale,
-        width = category.fv.width,
-        zoom = category.fv.zoom;
-
-    varCatViewer.varChart = ViewerHelper.createSVG(container, width, height, category.fv, 'up_pftv_variation-chart');
-
-    varCatViewer.variationCountArray = _.map(varCatViewer.features, function(d) {
-        return d.variants.length;
-    });
-
-    var varYScale = d3.scale.linear()
-                .domain([0,d3.max(varCatViewer.variationCountArray)])
-                .range([height, 0]);
-
-    var line = d3.svg.line()
-                .x(function(d,i) {
-                    return xScale(i);
-                })
-                .y(function(d) {
-                    return varYScale(d);
-                })
-                .interpolate('linear');
-
-    this.init = function () {
-        var varCatViewer = this;
-        varCatViewer.varChart.append("path")
-            .data(varCatViewer.features)
-            .attr("class","up_pftv_block-area")
-            .attr("d",line(varCatViewer.variationCountArray))
-            .on('click', function(){
-                category.toggle();
-            });
-
-        varCatViewer.varChart.append("path")
-            .data(varCatViewer.features)
-            .attr("class","up_pftv_line")
-            .attr("d",line(varCatViewer.variationCountArray))
-            .on('click', function(){
-                category.toggle();
-            });
-    };
-
-    this.init();
-
-    this.update = function() {
-        var varCatViewer = this;
-        varCatViewer.varChart.selectAll(".up_pftv_block-area")
-            .data(varCatViewer.features)
-            .attr("class","up_pftv_block-area")
-            .attr("d",line(varCatViewer.variationCountArray));
-
-        varCatViewer.varChart.selectAll(".up_pftv_line")
-            .data(varCatViewer.features)
-            .attr("class","up_pftv_line")
-            .attr("d",line(varCatViewer.variationCountArray));
-    };
-
-    this.updateData = function(data) {
-        var varCatViewer = this;
-        varCatViewer.features = data;
-
-        varCatViewer.variationCountArray = _.map(varCatViewer.features, function(d) {
-            return d.variants.length;
-        });
-
-        varCatViewer.varChart.selectAll(".up_pftv_block-area").remove();
-        varCatViewer.varChart.selectAll(".up_pftv_line").remove();
-        this.init();
-    };
-
-    return this;
 };
 
 //Category types
